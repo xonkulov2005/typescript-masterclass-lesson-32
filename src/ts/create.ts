23c3e100a71a5eb1855ddc5e67e5452ce5fb642d
Ismingiz <email@example.com>
2025-05-23 13:27:37 +0500

    Fix syntax error in newPizza object creation in form submission handler

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -5,12 +5,13 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
 
-const newPizza:PizzaModel{
-  title = formData.get("title") as string;
-  description = formData.get("description") as string;
-  toppings = formData.getAll("toppings") as string[];
-  price = parseInt(formData.get("price") as string);
-}
+  const newPizza: PizzaModel = {
+    title: formData.get("title") as string,
+    description: formData.get("description") as string,
+    toppings: formData.getAll("toppings") as string[],
+    price: parseInt(formData.get("price") as string),
+  };
+
   Pizza.create(newPizza).then(() => {
     window.location.href = "/";
   });
