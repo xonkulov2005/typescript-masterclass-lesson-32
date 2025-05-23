4d3e29d28decf872b6d93c56fdafa9cc3a932d20
Ismingiz <email@example.com>
2025-05-23 11:48:05 +0500

    Complate full page

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,19 +1,17 @@
 const form = document.querySelector(".create") as HTMLFormElement;
-import { Pizza } from "../model/model";
+import { Pizza, PizzaModel } from "../model/model";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(form);
 
-  const title = formData.get("title");
-  const description = formData.get("description");
-  const toppings = formData.getAll("toppings");
-  const price = formData.get("price");
-
-  Pizza.create({
-    title,
-    description,
-    toppings,
-    price,
+const newPizza:PizzaModel{
+  title = formData.get("title") as string;
+  description = formData.get("description") as string;
+  toppings = formData.getAll("toppings") as string[];
+  price = parseInt(formData.get("price") as string);
+}
+  Pizza.create(newPizza).then(() => {
+    window.location.href = "/";
   });
 });
